c76ba3a4b3b37a5d6b80b214724917bdde0dbd66
jarb29 <jarb29@gmail.com>
2020-09-21 19:12:09 -0300

    second

--- predict.py
+++ predict.py
@@ -85,9 +85,12 @@ def convert_and_pad(word_dict, sentence, pad=500):
 def convert_and_pad_data(word_dict, data, pad=500):
     result = []
     lengths = []
-    converted, leng = convert_and_pad(word_dict, sentence, pad)
-    result.append(converted)
-    lengths.append(leng)
+    
+    for sentence in data:
+        converted, leng = convert_and_pad(word_dict, sentence, pad)
+        result.append(converted)
+        lengths.append(leng)
+        
         
     return np.array(result), np.array(lengths)
 
